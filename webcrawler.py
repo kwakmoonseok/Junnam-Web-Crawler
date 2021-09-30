dc1428868159845fa8fc5ae62b624796a751f0d4
kwakmoonseok <48788722+kwakmoonseok@users.noreply.github.com>
2021-09-30 15:18:19 +0900

    Add files via upload

--- webcrawler.py
+++ webcrawler.py
@@ -1,3 +1,5 @@
+from os import close
+import re
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,9 +11,6 @@ with open('data.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 driver = webdriver.Chrome(executable_path='chromedriver.exe')
-
-# Chrome 창의 사이즈를 작게 하여 프로그램이 중단되지 않도록 함
-driver.set_window_position(-10000,0) 
 
 conn = pymysql.connect(host='localhost', user='root', password='1234', db='news', charset='utf8') 
 cursor = conn.cursor() 
@@ -26,6 +25,7 @@ class setting:
     TITLE = ''
     PAGE_CNT = 0
     PAGE_TEXT = ''
+    UNRELATED_ANNOUNCEMENT = ''
 
     def __init__(self, agency) -> None:
         self.NEXT = data[agency]['NEXT']
@@ -36,6 +36,7 @@ class setting:
         self.TITLE = data[agency]['TITLE']
         self.PAGE_CNT = data[agency]['PAGE_CNT']
         self.PAGE_TEXT = data[agency]['PAGE_TEXT']
+        self.UNRELATED_ANNOUNCEMENT = data[agency]['UNRELATED_ANNOUNCEMENT']
 
 
 
@@ -45,7 +46,9 @@ def crawler(link):
     elif 'naju' in link:
         agency = "나주 공고"
     elif 'jeonnam' in link:
-        agency = "전남 공고" 
+        agency = "전남 공고"
+    elif 'mokpo' in link:
+        agency = "목포 공고"
     
     driver.get(link)
     
@@ -56,40 +59,46 @@ def crawler(link):
     cnt = 1
     while driver.find_element_by_css_selector(page_info.NEXT):
         for _ in range(page_info.PAGE_CNT - 1):
-            for i in range(len(driver.find_elements_by_css_selector(page_info.NEWS_LIST))):
+            for i in range(len(driver.find_elements_by_css_selector(page_info.NEWS_LIST + page_info.UNRELATED_ANNOUNCEMENT))):
                 title, num, writed_date, collected_date, hyperlink = get_values_to_page(page_info, i)
                 
-                # 2021년의 데이터만 수집
-                if writed_date[:4] != '2021':
-                    return
+                print(writed_date)
+                if writed_date[:7] != '2021-09':
+                    if ':' in writed_date:
+                        writed_date = datetime.date.today()
+                    else:
+                        return
 
                 try:
                     cursor.execute(sql, (title, num, agency, writed_date, collected_date, hyperlink))
                 except pymysql.err.IntegrityError:
                     pass
                 conn.commit()
-                print(title) #debug 용
+
+                print(title) #debug
             cnt += 1
             driver.implicitly_wait(5)
             try:
                 driver.find_element_by_css_selector(page_info.PAGE_TEXT.format(cnt)).click()
             except selenium.common.exceptions.NoSuchElementException:
                 return
-        
-        # 더 이상 접근 가능한 페이지가 없을 경우 '다음'을 클릭하여 새로운 페이지 정보창으로 이동
+            
         driver.find_element_by_css_selector(page_info.NEXT).click()
         cnt += 1
+    
+    f.close()
+    driver.quit()
 
 def get_values_to_page(page_info, i):
     if str(type(page_info)) != "<class '__main__.setting'>" or str(type(i)) != "<class 'int'>": 
         raise TypeError('parameter type is different origin type')
 
-    news_list = driver.find_elements_by_css_selector(page_info.NEWS_LIST)
-    num = int(news_list[i].find_element_by_css_selector(page_info.NUM).text)
+    news_list = driver.find_elements_by_css_selector(page_info.NEWS_LIST + page_info.UNRELATED_ANNOUNCEMENT)
+    num = int(re.sub("\,", "", news_list[i].find_element_by_css_selector(page_info.NUM).text))
     writed_date = news_list[i].find_element_by_css_selector(page_info.WRITED_DATE).text
     collected_date = datetime.date.today()
+
     
-    # URL과 제목을 원본 값으로 얻기 위해 직접 클릭하여 수집 후 이전 페이지로 회귀
     driver.implicitly_wait(5)
     news_list[i].find_element_by_css_selector(page_info.GO_TO_MAIN_TEXT).click()
     title = driver.find_element_by_css_selector(page_info.TITLE).text
@@ -101,6 +110,6 @@ def get_values_to_page(page_info, i):
 
     return title, num, writed_date, collected_date, hyperlink
             
-links = [ "http://jcia.or.kr/cf/information/news.do", "https://www.naju.go.kr/www/administration/notice/financial", "https://www.jeonnam.go.kr/M7124/boardList.do?menuId=jeonnam0201000000"]
+links = [ "https://www.mokpo.go.kr/www/open_administration/city_news/notice", "http://jcia.or.kr/cf/information/news.do", "https://www.naju.go.kr/www/administration/notice/financial", "https://www.jeonnam.go.kr/M7124/boardList.do?menuId=jeonnam0201000000"]
 for link in links:
     crawler(link)
