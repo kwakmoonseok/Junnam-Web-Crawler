3371f6d4a0e4cc986e431b517a2a17b358eb27f4
kwakmoonseok <48788722+kwakmoonseok@users.noreply.github.com>
2021-10-05 15:57:22 +0900

    함수화 진행
    한 함수가 하나의 일을 하도록 함수 생성 및 모듈화

--- webcrawler.py
+++ webcrawler.py
@@ -1,11 +1,14 @@
-from os import close
+import datetime
+import json
 import re
+from os import close
+
+import pymysql
+
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common import keys
 from selenium.webdriver.common.keys import Keys
-import pymysql
-import datetime 
-import json
 
 with open('data.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
@@ -38,24 +41,8 @@ class setting:
         self.PAGE_TEXT = data[agency]['PAGE_TEXT']
         self.UNRELATED_ANNOUNCEMENT = data[agency]['UNRELATED_ANNOUNCEMENT']
 
-
-
 def crawler(link):
-    if 'jcia' in link:
-        agency = "JCIA 공고"
-    elif 'naju' in link:
-        agency = "나주 공고"
-    elif 'jeonnam' in link:
-        agency = "전남 공고"
-    elif 'mokpo' in link:
-        agency = "목포 공고"
-    elif 'yeosu' in link:
-        agency = "여수 공고"
-    elif 'suncheon' in link:
-        agency = "순천 공고"
-    elif 'gwangyang' in link:
-        agency = "광양 공고"
-    
+    agency = switch(link)
     driver.get(link)
     
     driver.implicitly_wait(5)
@@ -63,25 +50,21 @@ def crawler(link):
     page_info = setting(agency)
 
     cnt = 1
+
     while driver.find_element_by_css_selector(page_info.NEXT):
         for _ in range(page_info.PAGE_CNT - 1):
             for i in range(len(driver.find_elements_by_css_selector(page_info.NEWS_LIST + page_info.UNRELATED_ANNOUNCEMENT))):
-                title, num, writed_date, collected_date, hyperlink = get_values_to_page(page_info, i)
+                crawling_info = get_values_to_page(page_info, i)
+                crawling_info['agency'] = agency
                 
-                print(writed_date)
-                if writed_date[:7] != '2021-09':
-                    if ':' in writed_date:
-                        writed_date = datetime.date.today()
-                    else:
-                        return
-
-                try:
-                    cursor.execute(sql, (title, num, agency, writed_date, collected_date, hyperlink))
-                except pymysql.err.IntegrityError:
-                    pass
-                conn.commit()
-
-                print(title) #debug
+                current_date = checking_current_date(crawling_info['writed_date'])
+                if current_date:
+                    crawling_info['writed_date'] = current_date
+                else:
+                    return
+                
+                insert_sql(crawling_info)
+                
             cnt += 1
             driver.implicitly_wait(5)
             try:
@@ -94,6 +77,12 @@ def crawler(link):
     
     f.close()
     driver.quit()
+
+def switch(link):
+    site_category = { 'jcia' : "JCIA 공고", 'naju' : "나주 공고", 'jeonnam' : "전남 공고", 'mokpo' : "목포 공고", 'yeosu' : "여수 공고", 'suncheon' : "순천 공고", 'gwangyang' : "광양 공고" }
+    for key in site_category.keys():
+        if key in link:
+            return site_category[key]
 
 def get_values_to_page(page_info, i):
     if str(type(page_info)) != "<class '__main__.setting'>" or str(type(i)) != "<class 'int'>": 
@@ -113,9 +102,27 @@ def get_values_to_page(page_info, i):
         
     driver.implicitly_wait(5)
 
-    return title, num, writed_date, collected_date, hyperlink
-            
-links = [ "https://www.gwangyang.go.kr/board/list.gwangyang?boardId=BBS_0000004&menuCd=DOM_000000103001000000&contentsSid=227&cpath=", "https://www.suncheon.go.kr/kr/news/0001/0004/", "https://www.yeosu.go.kr/www/govt/news/notice", "https://www.mokpo.go.kr/www/open_administration/city_news/notice", "http://jcia.or.kr/cf/information/news.do", "https://www.naju.go.kr/www/administration/notice/financial", "https://www.jeonnam.go.kr/M7124/boardList.do?menuId=jeonnam0201000000"]
-for link in links:
-    crawler(link)
+    return {
+        'title': title,
+        'num' : num,
+        'writed_date' : writed_date,
+        'collected_date' : collected_date,
+        'hyperlink' : hyperlink
+    }
+
+def insert_sql(crawling_info):
+    try:
+        cursor.execute(sql, (crawling_info['title'], crawling_info['num'], crawling_info['agency'], crawling_info['writed_date'], crawling_info['collected_date'], crawling_info['hyperlink']))
+    except pymysql.err.IntegrityError:
+        pass
+    conn.commit()
+
+def checking_current_date(date):
+    if date[:7] != '2021-09':
+        if ':' in date:
+            return datetime.date.today()
+        return False
+sites = [ "https://www.gwangyang.go.kr/board/list.gwangyang?boardId=BBS_0000004&menuCd=DOM_000000103001000000&contentsSid=227&cpath=", "https://www.suncheon.go.kr/kr/news/0001/0004/", "https://www.yeosu.go.kr/www/govt/news/notice", "https://www.mokpo.go.kr/www/open_administration/city_news/notice", "http://jcia.or.kr/cf/information/news.do", "https://www.naju.go.kr/www/administration/notice/financial", "https://www.jeonnam.go.kr/M7124/boardList.do?menuId=jeonnam0201000000"]
+for site in sites:
+    crawler(site)
 driver.close()
